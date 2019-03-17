5b98fb43d409019d62769ccd2fadaccbe2be932f
Josh Wisenbaker <josh.wisenbaker@jamf.com>
2019-03-17 15:41:57 -0400

    Added TimeInterval extension

--- NoMAD-ADAuth/Extensions.swift
+++ NoMAD-ADAuth/Extensions.swift
@@ -50,28 +50,37 @@ extension String {
     /*
      
      // TODO: move this to UserInfo
+     
+     func variableSwap() -> String {
+     
+     var cleanString = self
+     
+     let domain = defaults.string(forKey: Preferences.aDDomain) ?? ""
+     let fullName = defaults.string(forKey: Preferences.displayName)?.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed) ?? ""
+     let serial = getSerial().addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed) ?? ""
+     let shortName = defaults.string(forKey: Preferences.userShortName) ?? ""
+     let upn = defaults.string(forKey: Preferences.userUPN) ?? ""
+     let email = defaults.string(forKey: Preferences.userEmail) ?? ""
+     
+     cleanString = cleanString.replacingOccurrences(of: "<<domain>>", with: domain)
+     cleanString = cleanString.replacingOccurrences(of: "<<fullname>>", with: fullName)
+     cleanString = cleanString.replacingOccurrences(of: "<<serial>>", with: serial)
+     cleanString = cleanString.replacingOccurrences(of: "<<shortname>>", with: shortName)
+     cleanString = cleanString.replacingOccurrences(of: "<<upn>>", with: upn)
+     cleanString = cleanString.replacingOccurrences(of: "<<email>>", with: email)
+     
+     return cleanString //.addingPercentEncoding(withAllowedCharacters: .alphanumerics)
+     
+     }
+     */
     
-    func variableSwap() -> String {
-        
-        var cleanString = self
-        
-        let domain = defaults.string(forKey: Preferences.aDDomain) ?? ""
-        let fullName = defaults.string(forKey: Preferences.displayName)?.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed) ?? ""
-        let serial = getSerial().addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed) ?? ""
-        let shortName = defaults.string(forKey: Preferences.userShortName) ?? ""
-        let upn = defaults.string(forKey: Preferences.userUPN) ?? ""
-        let email = defaults.string(forKey: Preferences.userEmail) ?? ""
-        
-        cleanString = cleanString.replacingOccurrences(of: "<<domain>>", with: domain)
-        cleanString = cleanString.replacingOccurrences(of: "<<fullname>>", with: fullName)
-        cleanString = cleanString.replacingOccurrences(of: "<<serial>>", with: serial)
-        cleanString = cleanString.replacingOccurrences(of: "<<shortname>>", with: shortName)
-        cleanString = cleanString.replacingOccurrences(of: "<<upn>>", with: upn)
-        cleanString = cleanString.replacingOccurrences(of: "<<email>>", with: email)
-        
-        return cleanString //.addingPercentEncoding(withAllowedCharacters: .alphanumerics)
-        
-    }
- */
-    
+}
+
+extension TimeInterval {
+    var seconds: TimeInterval { return self }
+    var minutes: TimeInterval { return self * 60 }
+    var hours: TimeInterval { return self * 3_600 }
+    var days: TimeInterval { return self * 86_400 }
+    var weeks: TimeInterval { return self * 604_800 }
+    var months: TimeInterval { return self * 2_628_000 }
 }
